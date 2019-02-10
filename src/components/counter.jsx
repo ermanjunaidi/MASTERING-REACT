ea6aa3b6eda6e9dac8fe7fe23dedd547b87301cb
unknown <ermanjunaidi1@gmail.com>
2019-02-10 19:05:42 +0700

    9- Components Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,31 +3,25 @@ import { tag } from 'postcss-selector-parser';
 
 class Counter extends Component {
     state = {
-        count: 2,
-        tags: ['tag1', 'tag2', 'tag3']
+        count: 0,
+        tags: []
+    };
+
+    renderTags() {
+        if (this.state.tags.lenght === 0) return <p>There are no tag</p>;
+
+        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
+
     }
     render() {
         return (
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increament</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                {this.state.tags.length === 0 && 'please crate a new tag'}
+                {this.renderTags()}
             </div>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? <h1>Zero</h1> : count
-    }
 }
 
 export default Counter;
