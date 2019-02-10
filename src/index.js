d41632e3ff57faa391e2454db7dd210fca2d81db
unknown <ermanjunaidi1@gmail.com>
2019-02-10 13:48:23 +0700

    7- Rendering Classes Dynamically

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Counter from './components/counter';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
