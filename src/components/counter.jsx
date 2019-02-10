9665ecdf3ef8e02a59eee11544a76a23e51ecf56
unknown <ermanjunaidi1@gmail.com>
2019-02-10 13:28:40 +0700

    6- Setting Attributes

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './bootstrap.css';
+
+class Counter extends Component {
+    state = {
+        count: 0
+    }
+    render() {
+        return (
+            <div>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increament</button>
+            </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? <h1>Zero</h1> : count
+    }
+}
+
+export default Counter;
