0741c85a22afc02b9d9bf174f440f22c84ae2722
unknown <ermanjunaidi1@gmail.com>
2019-02-10 20:19:54 +0700

    10- Components Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import { tag } from 'postcss-selector-parser';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        count: 0
     };
 
-    renderTags() {
-        if (this.state.tags.lenght === 0) return <p>There are no tag</p>;
-
-        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
-
+    handleIncrement() {
+        console.log('Increment Clicked', this);
     }
     render() {
         return (
             <div>
-                {this.state.tags.length === 0 && 'please crate a new tag'}
-                {this.renderTags()}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={this.handleIncrement}
+                    className="btn btn-secondary btn-sm">Increament</button>
             </div>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? <h1>Zero</h1> : count
+    }
 }
 
 export default Counter;
